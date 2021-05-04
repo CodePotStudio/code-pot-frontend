4aa41ffe13526370b6d492ed7d3a90a050116653
toycrane <alwaysfun2183@gmail.com>
2021-05-04 16:45:39 +0900

    잘못된 코드 삭제

--- src/pages/components/atoms/avatar/index.stories.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Avatar from "../avatar";
-
-export default {
-	title: "Atoms/Avatar",
-	component: [Avatar],
-};
-
-export const AvatarWithImage = () => <Avatar></Avatar>;
