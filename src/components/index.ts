d881b837cd820d06ecd498964121df8f5f26e4b3
toycrane <alwaysfun2183@gmail.com>
2021-05-06 20:06:35 +0900

    index page에 footer 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
+// Atoms
 export { default as Avatar } from "./atoms/avatar";
 export { default as Logo } from "./atoms/logo";
 export { default as Icon } from "./atoms/icon";
+
+// Organisms
+export { default as Footer } from "./organisms/footer";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "components";
 import Head from "next/head";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>Main</main>
-			<footer>footer</footer>
+			<Footer></Footer>
 		</div>
 	);
 }
