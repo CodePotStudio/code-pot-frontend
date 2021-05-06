1a00cd45f5a30bfc40e62f8ba7f82f0ea1f1ab3d
toycrane <alwaysfun2183@gmail.com>
2021-05-06 14:46:39 +0900

    space theming 추가

--- src/commons/style/themes/default.ts
+++ src/commons/style/themes/default.ts
@@ -86,6 +86,21 @@ const theme = {
       letter-spacing: 0.015rem;
     `,
 	},
+	space: [
+		"0.25rem",
+		"0.5rem",
+		"0.75rem",
+		"1rem",
+		"1.5rem",
+		"2rem",
+		"3rem",
+		"4rem",
+		"6rem",
+		"8rem",
+		"12rem",
+		"16rem",
+		"24rem",
+	],
 };
 
 export default theme;
