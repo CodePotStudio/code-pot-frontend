7b67883aa767642aea08f4e783cfc26e4e5c2fb4
toycrane <alwaysfun2183@gmail.com>
2021-05-06 18:07:39 +0900

    global Style, Theme storybook에 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
 import * as nextImage from "next/image";
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+import defaultTheme from "../src/commons/style/themes/default";
+import GlobalStyles from "../src/commons/style/GlobalStyle";
 
 // storybook에서 next/image 사용하도록 셋팅
 Object.defineProperty(nextImage, "default", {
@@ -15,3 +20,14 @@ export const parameters = {
 		},
 	},
 };
+
+// storybook에 theming 적용하기
+const themes = [defaultTheme];
+addDecorator(withThemesProvider(themes), ThemeProvider);
+
+// storybook에 global Style 적용하기
+addDecorator((story) => (
+	<>
+		<GlobalStyles /> {story()}
+	</>
+));
