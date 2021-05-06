317aec74bbc5fbe49007f644047e85e11e575923
toycrane <alwaysfun2183@gmail.com>
2021-05-06 15:47:42 +0900

    Next Image 사용하도록 설정 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,17 @@
+import * as nextImage from "next/image";
+
+// storybook에서 next/image 사용하도록 셋팅
+Object.defineProperty(nextImage, "default", {
+	configurable: true,
+	value: (props) => <img {...props} />,
+});
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
