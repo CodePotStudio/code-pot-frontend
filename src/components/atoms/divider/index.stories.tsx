e1613fb6d2250e901a230f071cff2e97044d80a3
toycrane <alwaysfun2183@gmail.com>
2021-05-07 17:29:56 +0900

    Divider 컴포넌트 구현

---
+++ src/components/atoms/divider/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Divider from ".";
+
+export default {
+	title: "Atoms/Divider",
+	component: [Divider],
+};
+
+export const Default = () => <Divider />;
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { default as Icon } from "./atoms/icon";
 export { default as Button } from "./atoms/button";
 export { default as Badge } from "./atoms/badge";
 export { default as CheckBox } from "./atoms/checkBox";
+export { default as Divider } from "./atoms/divider";
 
 // Organisms
 export { default as Footer } from "./organisms/footer";
