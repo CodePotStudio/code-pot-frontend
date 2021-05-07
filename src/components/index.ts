b8a5d189d5bed910a73d289aa907476ffb2538aa
toycrane <alwaysfun2183@gmail.com>
2021-05-07 16:40:42 +0900

    index에 badge 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Avatar } from "./atoms/avatar";
 export { default as Logo } from "./atoms/logo";
 export { default as Icon } from "./atoms/icon";
 export { default as Button } from "./atoms/button";
+export { default as Badge } from "./atoms/badge";
 
 // Organisms
 export { default as Footer } from "./organisms/footer";
