f34b79d21ae24cce2bc874f0f860fd2fe896361a
toycrane <alwaysfun2183@gmail.com>
2021-05-13 17:31:18 +0900

    molcule 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,17 +13,3 @@ export { default as Select } from "./atoms/select";
 export { default as Radio } from "./atoms/radio";
 export { default as Image } from "./atoms/image";
 export { Heading, Text } from "./atoms/typography";
-
-// molcules
-export { default as ClassCard } from "./molecules/classCard";
-export { default as ChallangeDescription } from "./molecules/challangeDescription";
-export { default as Drawer } from "./molecules/drawer";
-export { default as EpilogueCard } from "./molecules/epilogueCard";
-export { default as FormInput } from "./molecules/formInput";
-export { default as NavigationMenu } from "./molecules/navigationMenu";
-export { default as NavigationBar } from "./molecules/navigationBar";
-
-// Organisms
-export { default as Footer } from "./organisms/footer";
-export { default as DrawerGrid } from "./organisms/drawerGrid";
-export { default as EPCardGrid } from "./organisms/epilogueCardGrid";
