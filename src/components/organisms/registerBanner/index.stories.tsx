ec94b9ffde748717909efcd7fea6f11bc7e7b344
toycrane <alwaysfun2183@gmail.com>
2021-05-14 16:57:36 +0900

    register Banner 구현

---
+++ src/components/organisms/registerBanner/index.stories.tsx
@@ -0,0 +1,8 @@
+import RegisterBanner from ".";
+
+export default {
+	title: "Organisms/Register Banner",
+	component: [RegisterBanner],
+};
+
+export const Default = () => <RegisterBanner challangeId={1} />;
