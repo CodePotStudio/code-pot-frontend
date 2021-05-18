89b68fb413f1b0064e8155942c9d24836560a279
toycrane <alwaysfun2183@gmail.com>
2021-05-18 11:55:39 +0900

    main page 간격 조절

--- src/components/templates/main/style.ts
+++ src/components/templates/main/style.ts
@@ -4,20 +4,20 @@ import { palette } from "styled-tools";
 import { Heading } from "components/atoms/typography";
 
 export const HeroWrapper = styled(ContentContainer)`
-	padding: ${palette("spaces", 5)} 0px;
+	padding: ${palette("spaces", 7)} 0px;
 `;
 
 export const ChallangeGridWrapper = styled(ContentContainer)`
-	padding: ${palette("spaces", 5)} 0px;
+	padding: ${palette("spaces", 6)} 0px;
 	background-color: ${palette("grayscale", 6)};
 `;
 
 export const EpilogueGridWrapper = styled(ContentContainer)`
-	padding: ${palette("spaces", 5)} 0px;
+	padding: ${palette("spaces", 6)} 0px;
 	border-bottom: 1px solid ${palette("grayscale", 5)};
 `;
 export const FAQGridWrapper = styled(ContentContainer)`
-	padding: ${palette("spaces", 5)} 0px;
+	padding: ${palette("spaces", 6)} 0px;
 `;
 
 export const HeadingWrapper = styled(Heading)`
