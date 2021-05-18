2d1ce6d37774647fbcebdbe03b4f2d6f5077f14a
toycrane <alwaysfun2183@gmail.com>
2021-05-18 21:48:48 +0900

    불필요한 스타일 제거

--- src/components/organisms/challangeBanner/style.ts
+++ src/components/organisms/challangeBanner/style.ts
@@ -10,10 +10,8 @@ export const Wrapper = styled.div<CHBannerStyledProps>`
 	display: grid;
 	grid-gap: ${palette("spaces", 4)};
 	grid-template-columns: 1fr;
-	justify-content: center;
 	background-color: ${({ backgroundColor }) =>
 		backgroundColor ? backgroundColor : palette("grayscale", 2)};
-	padding: ${palette("spaces", 2)};
 	color: ${({ color }) => (color ? color : palette("white"))};
 	@media screen and (min-width: 32rem) {
 		grid-template-columns: 256px 1fr;
