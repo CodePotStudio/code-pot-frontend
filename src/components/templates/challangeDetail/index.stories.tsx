a591ca11cae2ddd2e43c03d6db1b4df8e2f13d08
toycrane <alwaysfun2183@gmail.com>
2021-05-18 11:09:27 +0900

    challange Detail template 추가

---
+++ src/components/templates/challangeDetail/index.stories.tsx
@@ -0,0 +1,24 @@
+import { CHBanner, RegisterBanner } from "components";
+import CHDetailTemplate from ".";
+import { DESCRIPTION } from "./constants";
+
+export default {
+	title: "Templates/Challange Detail Template",
+	component: [CHDetailTemplate],
+};
+
+export const Default = () => (
+	<CHDetailTemplate
+		descriptions={DESCRIPTION}
+		challangeBanner={
+			<CHBanner
+				id={1}
+				language="typescript"
+				status="inProgress"
+				to="/home"
+				start_at={new Date()}
+			/>
+		}
+		registerBannder={<RegisterBanner challangeId={1} />}
+	></CHDetailTemplate>
+);
