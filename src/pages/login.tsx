71031ce033c2e9316a7177f75f03430dcc8869e0
toycrane <alwaysfun2183@gmail.com>
2021-05-24 16:17:22 +0900

    login , signup URL 변경

--- src/pages/login.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginTemplate } from "components";
-import Seo from "components/molecules/Seo";
-
-export default () => (
-	<>
-		<Seo></Seo>
-		<LoginTemplate></LoginTemplate>
-	</>
-);
--- src/pages/signup.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupTemplate } from "components";
-import Seo from "components/molecules/Seo";
-
-export default () => (
-	<>
-		<Seo></Seo>
-		<SignupTemplate></SignupTemplate>
-	</>
-);
