f080936e8ba5396808d15c5d94a11a452f2ce5d2
toycrane <alwaysfun2183@gmail.com>
2021-05-25 10:13:20 +0900

    header에 accessToken 추가

--- src/apollo/authLink.ts
+++ src/apollo/authLink.ts
@@ -1,17 +1,19 @@
 import { setContext } from "@apollo/client/link/context";
-import { accessTokenCache } from "./cache";
+import { getSession } from "next-auth/client";
 
 type Headers = {
 	authorization?: string;
 };
 
 // 매 요청마다 토큰을 실어보내는 Link
-const authLink = setContext((_, { headers }: { headers: Headers }) => {
-	const accessToken = accessTokenCache();
+const authLink = setContext(async (_, { headers }: { headers: Headers }) => {
+	const session = await getSession();
 	const modifiedHeader = {
 		headers: {
 			...headers,
-			authorization: accessToken ? `Bearer ${accessToken}` : "",
+			authorization: session?.accessToken
+				? `Bearer ${session.accessToken}`
+				: "",
 		},
 	};
 	return modifiedHeader;
