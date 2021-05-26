33b6cbe54b5c2070aa451c6c7a13859e9aab20ba
toycrane <alwaysfun2183@gmail.com>
2021-05-26 14:42:34 +0900

    spinner 추가

--- src/components/templates/loading/index.tsx
+++ src/components/templates/loading/index.tsx
@@ -4,7 +4,7 @@ import * as S from "./style";
 const LoadingTemplate = () => {
 	return (
 		<S.Wrapper>
-			<S.StyledImage src="/spinner.svg"></S.StyledImage>
+			<S.StyledImage src="/spinner.gif"></S.StyledImage>
 		</S.Wrapper>
 	);
 };
