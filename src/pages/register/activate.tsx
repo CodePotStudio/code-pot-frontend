df3c40fe88723ee42e27a0a644fb2a962c0551c5
toycrane <alwaysfun2183@gmail.com>
2021-05-26 18:56:31 +0900

    activate Page 추가

--- src/pages/register/activate.tsx
+++ src/pages/register/activate.tsx
@@ -1,14 +1,35 @@
-import { ActivateAccountTemplate, ActivationForm } from "components";
-import Seo from "components/molecules/Seo";
+import { ActivateAccountTemplate, ActivationForm, Seo } from "components";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useActivateUserMutation } from "types/graphql/generated-types";
+import { ActivationFormValues } from "../../components/organisms/activationForm";
 
-export default () => (
-	<>
-		<Seo></Seo>
-		<ActivateAccountTemplate
-			title="추가 정보 입력하기"
-			activationForm={
-				<ActivationForm onSubmit={() => console.log("hello")}></ActivationForm>
-			}
-		></ActivateAccountTemplate>
-	</>
-);
+export default () => {
+	const router = useRouter();
+	const [activateUser, { data }] = useActivateUserMutation();
+	const handleSubmit = async ({
+		name,
+		mobile: originMobile,
+	}: ActivationFormValues) => {
+		const mobile = originMobile.replaceAll("-", "");
+		await activateUser({ variables: { name, mobile } });
+	};
+
+	useEffect(() => {
+		if (data) {
+			router.back();
+		}
+	});
+
+	return (
+		<>
+			<Seo></Seo>
+			<ActivateAccountTemplate
+				title="추가 정보 입력하기"
+				activationForm={
+					<ActivationForm onSubmit={handleSubmit}></ActivationForm>
+				}
+			></ActivateAccountTemplate>
+		</>
+	);
+};
