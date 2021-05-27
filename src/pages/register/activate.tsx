5860a15eaeac8742f1b4a60ed1420c7b1833f4fa
toycrane <alwaysfun2183@gmail.com>
2021-05-27 17:42:01 +0900

    activate 페이지 접근 권한 수정

--- src/pages/register/activate.tsx
+++ src/pages/register/activate.tsx
@@ -1,11 +1,24 @@
-import { ActivateAccountTemplate, ActivationForm, Seo } from "components";
+import {
+	ActivateAccountTemplate,
+	ActivationForm,
+	LoadingTemplate,
+	Seo,
+} from "components";
+import { getSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useActivateUserMutation } from "types/graphql/generated-types";
 import { ActivationFormValues } from "../../components/organisms/activationForm";
+import { Session } from "next-auth";
+import { GetServerSideProps } from "next";
 
-const activate = () => {
+interface Props {
+	session: Session | null;
+}
+
+const activate: React.FC<Props> = ({ session }) => {
 	const router = useRouter();
+	const isActive = session?.user?.isActive;
 	const [activateUser, { data }] = useActivateUserMutation();
 	const handleSubmit = async ({
 		name,
@@ -20,18 +33,35 @@ const activate = () => {
 			router.push("/");
 		}
 	});
+  // 비활성화일 시 홈으로 보내기
+	useEffect(() => {
+		if (isActive) {
+			router.push("/");
+		}
+	});
 
 	return (
 		<>
-			<Seo></Seo>
-			<ActivateAccountTemplate
-				title="추가 정보 입력하기"
-				activationForm={
-					<ActivationForm onSubmit={handleSubmit}></ActivationForm>
-				}
-			></ActivateAccountTemplate>
+			{isActive ? (
+				<LoadingTemplate />
+			) : (
+				<>
+					<Seo></Seo>
+					<ActivateAccountTemplate
+						title="추가 정보 입력하기"
+						activationForm={
+							<ActivationForm onSubmit={handleSubmit}></ActivationForm>
+						}
+					></ActivateAccountTemplate>
+				</>
+			)}
 		</>
 	);
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+	const session = await getSession(ctx);
+	return { props: { session } };
 };
 
 export default activate;
