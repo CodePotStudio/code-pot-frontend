ae055519d41f29ec932a4ea1754863edda2dadf0
toycrane <alwaysfun2183@gmail.com>
2021-06-03 15:34:56 +0900

    header sticky 옵션 추가

--- src/components/organisms/header/style.ts
+++ src/components/organisms/header/style.ts
@@ -1,6 +1,36 @@
-import styled from "styled-components";
+import { NONAME } from "node:dns";
+import styled, { css } from "styled-components";
+import { palette } from "styled-tools";
+import { largeContainerStyle } from "common/style/ContainerStyle";
 
-export const Wrapper = styled.div`
+export interface HeaderStyleProps {
+	sticky: boolean;
+}
+
+const StickyStyle = css`
+	border-bottom: 1px solid ${palette("grayscale", 5)};
+	position: fixed;
+	width: 100%;
+	left: 0;
+	top: 0;
+	z-index: 99999;
+	background-color: ${palette("white")};
+	box-shadow: 0px 0px 4px 0px rgb(0 0 0 / 6%);
+	padding: 0.75rem 0px;
+
+	& > div {
+		${largeContainerStyle};
+		margin: 0 auto;
+	}
+`;
+
+export const Wrapper = styled.div<HeaderStyleProps>`
+	${({ sticky }) => {
+		return sticky && StickyStyle;
+	}}
+`;
+
+export const NavBar = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
