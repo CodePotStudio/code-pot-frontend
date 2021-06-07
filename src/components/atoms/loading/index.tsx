9fc7e353ba74e042d00cbe7db36ffbeb6b210099
toycrane <alwaysfun2183@gmail.com>
2021-06-07 17:30:23 +0900

    Loading Atom 추가

---
+++ src/components/atoms/loading/index.tsx
@@ -0,0 +1,7 @@
+import { BaseTemplate } from "components";
+import * as S from "./style";
+
+const Loading = () => {
+	return <S.StyledImage src="/spinner.gif"></S.StyledImage>;
+};
+export default Loading;
---
+++ src/components/atoms/loading/style.ts
@@ -0,0 +1,8 @@
+import { Image } from "components";
+import styled from "styled-components";
+
+export const StyledImage = styled(Image)`
+	width: 48px;
+	height: 48px;
+	background-color: "";
+`;
