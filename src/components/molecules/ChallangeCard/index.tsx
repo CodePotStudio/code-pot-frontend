73f9d3989e34dae47c1cc6865a4b5921606a31f0
toycrane <alwaysfun2183@gmail.com>
2021-06-09 22:03:40 +0900

    graphql code generator 자동 실행하도록 변경

--- src/components/molecules/ChallangeCard/index.tsx
+++ src/components/molecules/ChallangeCard/index.tsx
@@ -16,6 +16,7 @@ const ChallangeCard = ({
 	status,
 	startDateTime,
 }: CHCardProps) => {
+	console.log(status);
 	const { variant, text: badgeText } = StatusSet[status];
 	return (
 		<S.CardLink to={routes.CHALLANGE_DETAIL(id)} key={id}>
