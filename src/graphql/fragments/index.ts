72cea2de80c4db8da21b29dc41e3cd55641d63ed
toycrane <alwaysfun2183@gmail.com>
2021-06-24 17:41:50 +0900

    enrollChallange mutation 추가

--- src/graphql/fragments/index.ts
+++ src/graphql/fragments/index.ts
@@ -1,2 +1,3 @@
 export { default as UserFields } from "./user.fragment";
 export { default as ChallangeFields } from "./challange.fragment";
+export { default as EnrollFields } from "./enroll.fragment";
