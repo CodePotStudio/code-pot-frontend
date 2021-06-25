ec738120443e444cf6def8353846c4d8063a83be
toycrane <alwaysfun2183@gmail.com>
2021-06-25 17:43:01 +0900

    안쓰는 타입 정리

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -12,21 +12,3 @@ type languageObj = {
 };
 
 export type variant = "primary" | "secondary" | "warning" | "alert";
-
-export interface ChallangeCardType {
-	id: string;
-	language: language;
-	status: status;
-	to: string;
-	start_at: Date;
-}
-
-export interface Challange {
-	id: number;
-	thumbnail: string;
-	name: string;
-	remarks: string;
-	status: status;
-	startDateTime: Date;
-	endDateTime: Date;
-}
